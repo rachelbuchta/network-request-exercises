aa1c8d2b064db57a14712b207b264442fdce1592
Rachel Buchta <rachelbuchta@yahoo.com>
2020-12-31 14:47:55 -0700

    Get data to post and delete from dom

---
+++ animal.js
@@ -0,0 +1,10 @@
+class Animal {
+    constructor(id, name, diet, fact) {
+    this.id = Date.now(),
+    this.name = name,
+    this.diet = diet,
+    this.fact = fact
+    }
+
+    
+}
--- index.js
+++ index.js
@@ -1,44 +1,62 @@
-// let newData = {
-//       id: 5,
-//       name: 'zebras',
-//       diet: 'grass, bugs',
-//       fun_fact: 'GAWGEOUS'
-//     }
-// seperate file with api calles being an object
-//import into here
-// get method, post, delete 
-fetch("http://localhost:3001/api/v1/animals", {
+  const idInput = document.querySelector('.id-input')
+  const nameInput = document.querySelector('.name-input')
+  const dietInput = document.querySelector('.diet-input')
+  const factInput = document.querySelector('.fact-input')
+  const animalName = document.querySelector('.animal-name')
+  const diet = document.querySelector('.diet')
+  const fact = document.querySelector('.fact')
+  const submitButton = document.querySelector('#submit-button')
+  const deleteButton = document.querySelector('#delete-button')
+  const animalsSection = document.querySelector('.animals')
+  const deleteId = document.querySelector('.id-input-delete')
+  
+submitButton.addEventListener('click', postAnimal)
+deleteButton.addEventListener('click', deleteAnimal)
+window.addEventListener('load', getAnimalData)
+
+
+function deleteAnimal() {
+const deleteAnimal = deleteId.value
+    fetch(`http://localhost:3001/api/v1/animals/${deleteAnimal}`, {
+  method: "DELETE"
+    })
+    location.reload()
+ }
+
+function postAnimal() {
+  fetch("http://localhost:3001/api/v1/animals", {
   method: "POST",
   headers: {
     "Content-Type": "application/json"
 },
   body: JSON.stringify({
-        id: 5,
-        name: 'zebras',
-        diet: 'grass, bugs',
-        fun_fact: 'GAWGEOUS'
-      })
+    id: parseInt(idInput.value),
+    name: nameInput.value,
+    diet: dietInput.value,
+    fun_fact: factInput.value
+    })
 })
-  .then(response => console.log(response))
+  .then(response => response.json())
   .then(json => console.log(json))
-  // .catch(err => console.log(err))
+  .catch(err => console.log(err))
 
+  getAnimalData()
+}
 
+function getAnimalData() {
   fetch("http://localhost:3001/api/v1/animals")
   .then(response => response.json())
   .then(animals => showAnimals(animals))
+}
 
-  showAnimals = animals => {
-
-  const animalsSection = document.querySelector(".animals")
-
+  function showAnimals(animals) {
   animals.map(animal => {
-    const animalElement = document.createElement("p")
-
-   animalElement.innerText = `Animal Name: ${animal.name},
+  const animalElement = document.createElement("p")
+   animalElement.innerText = `
+   ID: ${animal.id},
+   Animal Name: ${animal.name},
         Diet: ${animal.diet},
         Fun Fact: ${animal.fun_fact}`
-
-     animalsSection.append(animalElement)
+     animalsSection.appendChild(animalElement)
   })
 }
